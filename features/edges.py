223605fee70612fc4b2c8f497e817944e4cb1ad2
Hernan Contigiani <61872731+hernancontigiani@users.noreply.github.com>
2022-11-04 11:27:16 -0300

    Edge only feature

--- features/edges.py
+++ features/edges.py
@@ -3,8 +3,10 @@ import cv2
 import numpy as np
 
 class EdgeDetector():
-    def __init__(self):
+    def __init__(self, edge_only=False):
+        self.edged = None
         self.contornos = None
+        self.edge_only = edge_only
     
     def __call__(self, image):
         t0 = time.time()
@@ -15,10 +17,10 @@ class EdgeDetector():
         # Aplicar algortimo Canny para encontrar bordes
         # Hay que ajustar los filtros min y max según el
         # fondo y la luz
-        edged = cv2.Canny(gray, 50, 400)
+        self.edged = cv2.Canny(gray, 50, 150)
         
         # Encontrar los contornos formados por esos bordes
-        contours, hierarchy = cv2.findContours(edged, 
+        contours, hierarchy = cv2.findContours(self.edged, 
             cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
         
         self.contornos = contours
@@ -28,7 +30,8 @@ class EdgeDetector():
 
     def draw(self, image):
         # Dibujar todos los contornos en la imagen
-        if self.contornos is not None:
+        if self.edge_only == False and self.contornos is not None:
             cv2.drawContours(image, self.contornos, -1, (0, 255, 0), 1)
+        if self.edge_only == True and self.edged is not None:
+            image = self.edged
         return image
-
